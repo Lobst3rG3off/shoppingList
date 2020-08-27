e5939c597c206d33016d969213b461ad10c162fb
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-08-27 17:56:40 -0400

    pushing items from input into the empty array

--- shopping.js
+++ shopping.js
@@ -2,6 +2,25 @@ const shoppingForm = document.querySelector('.shopping');
 const list = document.querySelector('.list');
 
 //make an array to hold state
+const items = [];
+
+function handleSubmit(e){
+    e.preventDefault();
+    const name = e.currentTarget.item.value;
+    const item = {
+        name,
+        id: Date.now(),
+        complete: false
+    };
+    //push the items into state
+    items.push(item);
+    console.log(`There are now ${items.length} in your state`)
+    //clear the form 
+    e.target.reset();
+}
+
+
+shoppingForm.addEventListener('submit', handleSubmit);
 
 
 
